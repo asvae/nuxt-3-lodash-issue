79188bca41c87205207f92cc405ddd01d86c90ba
Yauheni Prakopchyk <yauheni.prakopchyk@epicmax.co>
2022-04-06 16:43:51 +0400

    init

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button @click="count++">{{ count }}</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import sortBy from 'lodash/sortBy'
+
+console.log(sortBy)
+
+const count = ref(0)
+</script>
